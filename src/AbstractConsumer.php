1e5f53bc0a976cb8dcd6d125e7e4ae99056b5923
grp <gregoire57710@hotmail.fr>
2018-01-25 10:25:48 +0100

    Fix Consumer constructor

--- src/AbstractConsumer.php
+++ src/AbstractConsumer.php
@@ -2,20 +2,49 @@
 
 namespace Mouf\AmqpClient;
 
+use Doctrine\ORM\ORMException;
+use Mouf\Utils\Log\ErrorLogLogger;
+use Psr\Log\LoggerInterface;
+
 abstract class AbstractConsumer implements ConsumerInterface
 {
     use ConsumerTrait;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * AbstractConsumer constructor.
+     * @param LoggerInterface|null $logger
+     */
+    public function __construct(LoggerInterface $logger = null)
+    {
+        $this->logger = $logger ?: new ErrorLogLogger();
+    }
 
     public function callback($msg)
     {
         try {
             $this->onMessageReceived($msg);
-
             $msg->delivery_info['channel']->basic_ack($msg->delivery_info['delivery_tag']);
         } catch (RetryableExceptionInterface $e) {
+            $this->logger->error("Exception caught while consuming message.", [
+                'exception' => $e
+            ]);
             $msg->delivery_info['channel']->basic_nack($msg->delivery_info['delivery_tag'], true, true);
+            if ($e instanceof FatalExceptionInterface) {
+                throw $e;
+            }
         } catch (\Exception $e) {
+            $this->logger->error("Exception caught while consuming message.", [
+                'exception' => $e
+            ]);
             $msg->delivery_info['channel']->basic_nack($msg->delivery_info['delivery_tag'], true, false);
+            if ($e instanceof FatalExceptionInterface) {
+                throw $e;
+            }
         }
     }
 
